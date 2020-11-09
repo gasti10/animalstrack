fe7bb0f80730ca0afcf1ea001229f59a6ba71e18
gasti10 <gastonjouglard.10@gmail.com>
2020-11-09 01:59:37 -0300

    Update urls.py
    ADD -> path('', portfolio.views.index),

--- portalsite/urls.py
+++ portalsite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', portfolio.views.index),
     path('portfolio/', include('portfolio.urls')),
     path('admin/', admin.site.urls),
 ]
